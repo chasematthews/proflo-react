5d627744b93fdfee3c54aa5ad61c004f8fed3d45
Chase Matthews <chase.matt@outlook.com>
2023-02-07 14:53:17 +1100

    document querying now available

--- src/components/Project/PFD/Drawing.js
+++ src/components/Project/PFD/Drawing.js
@@ -1,20 +1,49 @@
 import React, {useState, useEffect} from 'react';
 import styles from '../../../styles/Project.module.css'
+import StreamTable from './StreamTable';
 
 const Drawing = () => {
     const[htmlFileString, setHtmlFileString] = useState();
+    const[streamNumbersList, setStreamNumbersList] = useState([]);
 
     async function fetchHtml() {
         setHtmlFileString(await ( await fetch('./resources/drawing.html')).text());
+    }
+
+    const getElements = () => {
+        const identifiers = document.getElementById("PFD").querySelectorAll("div");
+        let streamNumbers = findStreamNumbers(identifiers);
+        setStreamNumbersList(streamNumbersList.concat(streamNumbers));
+    }
+    
+    const findStreamNumbers = (identifiers) => {
+        let streamNumbers = [];
+        identifiers.forEach(identifier => {
+            if (identifier.textContent.substring(0,2) === "AS") {
+                streamNumbers.push((identifier));
+            }        
+        });
+        return streamNumbers;
     }
 
     useEffect(() => {
         fetchHtml()
     }, [])
 
+    useEffect(() => {
+        getElements();
+    }, [htmlFileString])
+
     return (
         <div className={styles.main}>
-            <div className={styles.PFDWrapper} dangerouslySetInnerHTML={{ __html: htmlFileString }}></div>
+            <div id="PFD" className={styles.PFDWrapper} dangerouslySetInnerHTML={{ __html: htmlFileString }}></div>
+            {streamNumbersList.map((streamNumber) => {
+                return (
+                    <StreamTable
+                        streamNumber = {streamNumber}
+                    />
+                )
+            })}
         </div>
     )
 }
---
+++ src/components/Project/PFD/StreamTable.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const StreamTable = ({ streamNumber }) => {
+    console.log(streamNumber.textContent)
+    return (
+        <div>
+            Hello
+        </div>
+    )
+}
+
+export default StreamTable
