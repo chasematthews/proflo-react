5146eae5853d649e55423fbe1c776822d2978336
Chase Matthews <chase.matt@outlook.com>
2023-02-10 11:52:56 +1100

    image upload now working

--- src/components/Home/HomeNav.js
+++ src/components/Home/HomeNav.js
@@ -1,6 +1,8 @@
 import React, {useState, useRef} from 'react';
 import styles from '../../styles/Home.module.css'
 import SidebarRow from './SidebarRow';
+import { getAuth } from 'firebase/auth'
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage'
 
 //Import all of the required icons for the side bar
 import ContentPasteIcon from '@mui/icons-material/ContentPaste';
@@ -12,9 +14,22 @@ const HomeNav = ({toggleProjectModalHandleClick}) => {
     const [companyLogo, setCompanyLogo] = useState();
     const buttonUploadRef = useRef()
 
-    const addCompanyLogo = (event) => {
+    console.log(companyLogo)
+
+    const toggleUploadDialogue = (event) => {
         event.preventDefault();
         buttonUploadRef.current.click();
+    }
+
+    const onMediaFileSelected = async(event) => {
+        event.preventDefault();
+        let file = event.target.files[0];
+        const filePath = `${getAuth().currentUser.uid}/${file.name}`;
+        const newImageRef = ref(getStorage(), filePath);
+        await uploadBytesResumable(newImageRef, file);
+
+        const publicImageUrl = await getDownloadURL(newImageRef)
+        setCompanyLogo(publicImageUrl)
     }
 
     const sidebarStyle = styles.sidebarRow
@@ -29,10 +44,10 @@ const HomeNav = ({toggleProjectModalHandleClick}) => {
         <div className={styles.navigator}>
             <div className={styles.companyNameWrapper}>
                 {companyLogo ? 
-                    (<div>Hello</div>) :
+                    (<div><img src={`${companyLogo}`} alt='company logo' /></div>) :
                     (<div>
-                        <button className={styles.uploadFileButton} onClick={addCompanyLogo}>Add File</button>
-                        <input className={styles.uploadFileInput} ref={buttonUploadRef} type="file" />
+                        <button className={styles.uploadFileButton} onClick={(event) => toggleUploadDialogue(event)}>Add File</button>
+                        <input className={styles.uploadFileInput} ref={buttonUploadRef} onChange={(event) => onMediaFileSelected(event)} type="file" />
                     </div>
                     )   
                 }
