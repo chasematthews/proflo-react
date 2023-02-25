dd27fdea5b966357bbe864bc479717cf1ac73d64
Chase Matthews <chase.matt@outlook.com>
2023-02-25 17:00:07 +1100

    preview is now working and the CSS is touched up for the project dashboard

--- src/components/Home/Project/ProjectCard.js
+++ src/components/Home/Project/ProjectCard.js
@@ -3,11 +3,14 @@ import styles from '../../../styles/Home.module.css';
 import { useNavigate } from 'react-router-dom';
 import {Document, Page} from 'react-pdf/dist/esm/entry.webpack5'
 import { doc, getDoc, getFirestore, getCollection, onSnapshot, query, collection } from 'firebase/firestore';
+import { UserAuth } from '../../../contexts/AuthContext';
 
 const ProjectCard = ({project, team}) => {
 
     const[imageURL, setImageURL] = useState('')
-    console.log(imageURL)
+
+    //import the user database from the AuthContext
+    const {userRef} = UserAuth();
 
     const navigate = useNavigate();
 
@@ -20,12 +23,21 @@ const ProjectCard = ({project, team}) => {
     }
 
     const getProjectImages = async() => {
-        const teamDB = await doc(getFirestore(), 'teams', `${team.id}`)
-        const projectDB = await doc(teamDB, 'projects', `${project.name}`)
-        const docQuery = query(collection(projectDB, 'documents'))
-        onSnapshot(docQuery, (snapshot) => {
-            setImageURL(snapshot.docs[1].data().screenGrab)
-        })
+        let projectDB;
+        if (team === undefined) {
+            projectDB = await doc(userRef, 'projects', `${project.name}`)
+            const docQuery = query(collection(projectDB, 'documents'))
+            onSnapshot(docQuery, (snapshot) => {
+                setImageURL(snapshot.docs[0].data().screenGrab)
+            })
+        } else {
+            const teamDB = await doc(getFirestore(), 'teams', `${team.id}`)
+            projectDB = await doc(teamDB, 'projects', `${project.name}`)
+            const docQuery = query(collection(projectDB, 'documents'))
+            onSnapshot(docQuery, (snapshot) => {
+                setImageURL(snapshot.docs[0].data().screenGrab)
+            })
+        }
     }
 
     useEffect(() => {
@@ -36,7 +48,7 @@ const ProjectCard = ({project, team}) => {
         <div className={styles.projectCard} onClick={goToProject}>
             <div className={styles.projectImageWrapper}>
                 {imageURL ? 
-                    <img src={`${imageURL}`} alt='Document Preview' /> :
+                    <img className={styles.previewImage} src={`${imageURL}`} alt='Document Preview' /> :
                     // <object data={`${project.PDFURL}`} type="application/pdf" width="100%" height="100%">
                     //     <p>Alternative text - include a link <a href={`${project.PDFURL}`}>to the PDF!</a></p>
                     // </object> :
